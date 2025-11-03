a87708168c374f6c5537a844e7b773e267bfd5bc
CrazyDevil3 <161240345+CrazyDevil3@users.noreply.github.com>
2025-11-03 23:54:38 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ export default function Footer() {
   return (
     <footer className="py-8 text-center text-slate-500">
       <div className="max-w-6xl mx-auto px-4">
-        © {new Date().getFullYear()} Srijal Yadav
+        © {new Date().getFullYear()} Rakshit Jain
       </div>
     </footer>
   );
 }
+
