c94c6aaa139f216bcd0e2f8d3364ee9b297768af
CrazyDevil3 <161240345+CrazyDevil3@users.noreply.github.com>
2025-11-03 23:56:01 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,12 @@ export default function About() {
       <div className="max-w-3xl mx-auto text-center">
         <h2 className="text-3xl font-semibold text-[var(--primary)]">About Me</h2>
         <p className="mt-4 text-slate-700 leading-relaxed">
-       Passionate and dedicated Unity game developer with hands-on experience building games. Proficient in
-implementing game mechanics, UI systems, and visual design with tools such as Unity and Canva. Completed a
-fully functional solo-developed 2D game and is currently working on another project, demonstrating a strong
-foundation in problem solving and game logic. Interested in self-driven learning and collaboration..
+      Highly motivated and detail-oriented Fresher Developer with a strong foundation in programming, problem-solving, and software development 
+principles. Proficient in languages with hands-on experience in building some projects. Eager to contribute to real-world software solutions, 
+learn from experienced developers, and grow in a collaborative tech environment.
         </p>
       </div>
     </section>
   );
 }
+
